14bd505cbf497e6d16fb7ce991f2c44e398a2223
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-03-17 09:46:47 +0530

    fix: body overflow
    Signed-off-by: sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,12 @@ export default function Home() {
   return (
     <>
       {/* <JsonInput /> */}
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 w-screen">
         <div className="flex">
           <FileTree />
           <YAMLInput />
         </div>
-        <div className="relative w-full">
-          <div className="absolute w-full">
-            <XTerm />
-          </div>
-        </div>
+        <XTerm />
       </div>
 
       {/* <CodeSandBoxEmbed /> */}
